87956f99dff07fa27644dab84fb38576e3a6bdc7
202510-aw-sugihara <202510_aw_sugihara@x-ide.sakura.ne.jp>
2026-02-09 14:01:44 +0900

    Spring Initializr追加

---
+++ qc-logbook/src/main/java/com/example/qc_logbook/QcLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.qc_logbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcLogbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcLogbookApplication.class, args);
+	}
+
+}
---
+++ qc-logbook/src/test/java/com/example/qc_logbook/QcLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qc_logbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcLogbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
